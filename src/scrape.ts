856b50a00e294c3586da73c2ca1f37c0bb8bcaf0
mik-siv <mikhail.sivinskikh@bwt.ee>
2022-04-20 17:40:57 +0300

    wip

--- src/scrape.ts
+++ src/scrape.ts
@@ -3,6 +3,7 @@ import {reqPage} from './requests';
 import {CryptoRowData, CryptoRow} from "./models";
 
 const scrape = async (url: string): Promise<void> => {
+    const baseUrl = 'https://finance.yahoo.com'
     let page
     try {
         page = await reqPage(url);
@@ -22,9 +23,10 @@ const scrape = async (url: string): Promise<void> => {
         price: "price (intraday)",
         previousClosePrice: "previous close"
     }
+
     const $ = cheerio.load(page.data);
     const result: CryptoRowData[] = [];
-    $('tbody .simpTblRow').each((i, el) => {
+    $('tbody .simpTblRow').each(async (i, el) => {
         let row: {} = {};
         $(el).find("td").each((i, el) => {
             for (const iter in columnNames) {
@@ -33,10 +35,22 @@ const scrape = async (url: string): Promise<void> => {
                 }
             }
         })
+        let linkAddress = $(el).find('[data-test="quoteLink"]').attr("href");
+        row.link = baseUrl + linkAddress;
+        let subPage;
+        try {
+            subPage = await reqPage(row.link);
+        } catch (err) {
+            console.log(err);
+            return;
+        }
+        const pageData = cheerio.load(subPage.data)
+        row.previousClosePrice = pageData('#Main').find('[data-test="PREV_CLOSE-value"]').text()
+        row.openPrice = pageData('#Main').find('[data-test="OPEN-value"]').text()
+        row.daysRange = pageData('#Main').find('[data-test="DAYS_RANGE-value"]').text()
         result.push(row)
+        console.log(result.length)
     })
-
-    console.log(JSON.stringify(result))
 
 
     //1. read table rows
@@ -51,6 +65,6 @@ const scrape = async (url: string): Promise<void> => {
 }
 
 (async () => {
-    const startingUrl = 'https://finance.yahoo.com/cryptocurrencies/';
+    const startingUrl = 'https://finance.yahoo.com/cryptocurrencies';
     await scrape(startingUrl)
 })();
